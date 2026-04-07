34d49b09f6d41b0d16412783eb9c52fb335541c8
elvis.silva <elvis@onetouchhealth.net>
2026-04-06 22:40:54 -0300

    created factory for service

---
+++ database/factories/ServiceFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Service;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Service>
+ */
+class ServiceFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->words(3, true),
+            'duration' => $this->faker->randomElement(['30 min', '1 hour', '2 hours']),
+        ];
+    }
+}
